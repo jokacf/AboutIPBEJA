808d03d7f92e68657e8983f8f0f6450c9ee51aeb
Joaquim <16487@alunos.ipbeja.pt>
2017-11-22 21:31:06 +0000

    Criacao das pastas que permitem a compatibilidade da aplicacao com várias telas inclusive landscape, codigo que permite saber a orientacao da aplicacao, portrait ou landscape, e customizacao incompleta da nossa Toolbar

--- app/src/main/java/com/example/user/aboutipbeja/MainActivity.java
+++ app/src/main/java/com/example/user/aboutipbeja/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.user.aboutipbeja;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
+        setSupportActionBar(toolbar);
+
+        int displaymode = getResources().getConfiguration().orientation;
+        if (displaymode == 1) { //portrait mode
+            Toast.makeText(this, "PORTRAITT EHEHE", Toast.LENGTH_SHORT).show();
+        } else {//landscape
+            Toast.makeText(this, "LANDSCAPE EHEHE", Toast.LENGTH_SHORT).show();
+        }
     }
 }
