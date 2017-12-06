b81de533378a7695c347a6689f10f5b1477bd808
Joaquim <16487@alunos.ipbeja.pt>
2017-12-06 12:12:03 +0000

    Criacao da classe IPBejaData que contem as informacoes a representar na listview e os respetivos toast para verificar o funcionamento

--- app/src/main/java/com/example/user/aboutipbeja/FragmentHeadlines.java
+++ app/src/main/java/com/example/user/aboutipbeja/FragmentHeadlines.java
@@ -1,11 +1,13 @@
 package com.example.user.aboutipbeja;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 /**
@@ -15,15 +17,25 @@ public class FragmentHeadlines extends Fragment {
 
 
     public FragmentHeadlines() {
-        // Required empty public constructor
-    }
 
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_headlines, container, false);
+
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[0] + "Historia" + IPBejaData.Artigos[0], Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[1] + "Historia" + IPBejaData.Artigos[1], Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[2] + "Historia" + IPBejaData.Artigos[2], Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[3] + "Historia" + IPBejaData.Artigos[3], Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/example/user/aboutipbeja/IPBejaData.java
@@ -0,0 +1,26 @@
+package com.example.user.aboutipbeja;
+
+/**
+ * Created by user on 06/12/2017.
+ */
+
+public class IPBejaData {
+    static String[] Escolas = {
+            "Escola Superior de Tecnologia e Gestão",
+            "Escola Superior de Saúde",
+            "Escola Superior Agrária",
+            "Escola Superior de Educação"
+    };
+    static String[] Artigos = {
+            "Escola Superior de Tecnologia e Gestão\n\nA ESTIG foi criada em 1991 (Decreto-Lei nº 40791, de 21 de Janeiro). \nNeste diploma determinava-se que as " +
+                    "suas actividades fossem financiadas por receitas próprias, subsídios, comparticipações de empresas e "+
+            "de câmaras localizadas na área geográfica de influência da escola.",
+            "Escola Superior de Saúde\n\nCriação da Escola de Enfermagem de Beja através do Decreto nº 569/73, de 30 de Outubro.\n" +
+                    "Em Outubro de 1975, iniciou as suas actividades lectivas, em edifício próprio, com um total de 70 alunos.\n" +
+                    "A partir de 15 de Setembro de 1989 passou a designar-se Escola Superior de Enfermagem de Beja,\n" +
+                    "tendo sido integrada na rede nacional do ensino superior politécnico desde 23 de Dezembro de 1988 (Dec.-Lei nº 480/88).",
+            "Escola Superior Agrária\n\n Conteudo a introduzir brevemente",
+            "Escola Superior de Educação\n\nA ESEB deu início aos primeiros cursos no ano lectivo de 1986/87, processo esse articulado com a extinção da Escola do Magistério Primário de Beja nesse mesmo ano.\n" +
+                    " As instalações da Escola do Magistério, pertencentes à Diocese de Beja, continuaram a ser utilizadas pela ESEB, até à construção de instalações próprias que vieram a ser ocupadas em Janeiro de 1994."
+    };
+}
