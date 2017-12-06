7f51adc50e6ea96c96604ac049398366b77d62e7
Joaquim <16487@alunos.ipbeja.pt>
2017-12-06 12:35:31 +0000

    Transformacao da Classe FragmentHeadlines numa lista e criacao do metedo onListItemClick que permite saber qual elemento da lista foi clickado

--- app/src/main/java/com/example/user/aboutipbeja/FragmentHeadlines.java
+++ app/src/main/java/com/example/user/aboutipbeja/FragmentHeadlines.java
@@ -4,16 +4,21 @@ package com.example.user.aboutipbeja;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentHeadlines extends Fragment {
+public class FragmentHeadlines extends ListFragment {
 
 
     public FragmentHeadlines() {
@@ -24,18 +29,19 @@ public class FragmentHeadlines extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_headlines, container, false);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, IPBejaData.Escolas);
 
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[0] + "Historia" + IPBejaData.Artigos[0], Toast.LENGTH_SHORT).show();
-        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[1] + "Historia" + IPBejaData.Artigos[1], Toast.LENGTH_SHORT).show();
-        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[2] + "Historia" + IPBejaData.Artigos[2], Toast.LENGTH_SHORT).show();
-        Toast.makeText(getActivity(), "" +IPBejaData.Escolas[3] + "Historia" + IPBejaData.Artigos[3], Toast.LENGTH_SHORT).show();
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (getActivity().findViewById(R.id.fragment_container) != null) {
+            Toast.makeText(getActivity().getBaseContext(), "Clicked on: " + IPBejaData.Escolas[position],
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
