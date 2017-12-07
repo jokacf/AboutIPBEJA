a59a88c1bc876d3d520fa8346baddffef3b7a45b
Joaquim <16487@alunos.ipbeja.pt>
2017-12-07 16:39:43 +0000

    Criacao de um menu presente na toolbar que ira permitir efetuar uma chamada para o ipbeja, visitar a pagina do ipbeja e aceder a localizacao do ipbeja atraves do google maps

--- app/src/main/java/com/example/user/aboutipbeja/MainActivity.java
+++ app/src/main/java/com/example/user/aboutipbeja/MainActivity.java
@@ -4,15 +4,23 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    boolean isOpen = false;
+    FloatingActionButton fab_menu;
+    ImageView  fab_call, fab_map, fab_web;
+    Animation fabOpen, fabClose, fabRright, fabRleft;
     Intent call;
     static final int PICK_CONTACT_REQUEST = 1;
 
@@ -22,6 +30,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.customToolbar);
         setSupportActionBar(toolbar);
+
+        fab_menu = (FloatingActionButton) findViewById(R.id.menuFloating);
+        fab_call = (ImageView) findViewById(R.id.call_image);
+        fab_map = (ImageView) findViewById(R.id.map_image);
+        fab_web = (ImageView) findViewById(R.id.web_image);
+
+        fabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.menu_open);
+        fabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.menu_close);
+        fabRright = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_right);
+        fabRleft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_left);
+
+        fab_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isOpen){
+                    fab_menu.startAnimation(fabRleft);
+                    fab_call.startAnimation(fabClose);
+                    fab_map.startAnimation(fabClose);
+                    fab_web.startAnimation(fabClose);
+                    fab_call.setClickable(false);
+                    fab_map.setClickable(false);
+                    fab_web.setClickable(false);
+                    isOpen = false;
+                }else{
+                    fab_menu.startAnimation(fabRright);
+                    fab_call.startAnimation(fabOpen);
+                    fab_map.startAnimation(fabOpen);
+                    fab_web.startAnimation(fabOpen);
+                    fab_call.setClickable(true);
+                    fab_map.setClickable(true);
+                    fab_web.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+
+        fab_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri number = Uri.parse("tel:+351 284 315 000");
+                call = new Intent(Intent.ACTION_CALL, number);
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    call = new Intent(Intent.ACTION_DIAL, number);
+                }
+                startActivityForResult(call, PICK_CONTACT_REQUEST);
+            }
+        });
+
+        fab_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri webpage = Uri.parse("http://www.ipbeja.pt");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        fab_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri location = Uri.parse("geo:38.0135135,-7.8734867?z=17");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startActivity(mapIntent);
+            }
+        });
 
         int displaymode = getResources().getConfiguration().orientation;
         if (displaymode == 1) { //portrait mode
@@ -43,14 +116,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Landscape!", Toast.LENGTH_SHORT).show();
         }
     }
-
-    public void call(View view) {
-        Uri number = Uri.parse("tel:12044");
-        call = new Intent(Intent.ACTION_CALL, number);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            call = new Intent(Intent.ACTION_DIAL, number);
-        }
-        startActivityForResult(call, PICK_CONTACT_REQUEST);
-}
 }
 
