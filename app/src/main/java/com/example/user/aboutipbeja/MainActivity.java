4f80fa5d72cdf749f9b5d74f85a2248412d19c94
Joaquim <16487@alunos.ipbeja.pt>
2017-12-07 02:34:05 +0000

    Criacao da possibilidade de telefonar para o IPbeja atraves da app

--- app/src/main/java/com/example/user/aboutipbeja/MainActivity.java
+++ app/src/main/java/com/example/user/aboutipbeja/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.user.aboutipbeja;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    Intent call;
+    static final int PICK_CONTACT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +43,14 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Landscape!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void call(View view) {
+        Uri number = Uri.parse("tel:12044");
+        call = new Intent(Intent.ACTION_CALL, number);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            call = new Intent(Intent.ACTION_DIAL, number);
+        }
+        startActivityForResult(call, PICK_CONTACT_REQUEST);
 }
+}
+
